3f861424f788b28ce29b0600add9c0141cab1cdd
mnodhe <mnodhe@gmail.com>
2024-01-27 11:55:27 +0330

    init alien class

---
+++ src/main/java/com/mnodhe/Main.java
@@ -0,0 +1,10 @@
+package com.mnodhe;
+
+public class Main {
+    public static void main(String[] args) {
+        Alien telusko = new Alien();
+        telusko.setAid(101);
+        telusko.setAname("navin");
+        telusko.setColor("Green");
+    }
+}
